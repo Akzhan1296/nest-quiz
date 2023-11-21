b0c9a71521a3ae02881409bd5242ddd20b7cf546
Akzhan Sarzhanuly <akzhansarzhanuly@Air-Akzhan-2.lan>
2023-11-21 11:31:09 +0600

    feat(21.11.2023): init nest

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres ',
+      database: 'postgres',
+      autoLoadEntities: false,
+      synchronize: false,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
